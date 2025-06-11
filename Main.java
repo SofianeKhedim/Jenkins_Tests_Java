e5e9bab60f6c44cfbf8432a1753a3c249cb6a4f0
Sofiane KHEDIM <55292902+SofianeKhedim@users.noreply.github.com>
2025-06-11 10:39:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,16 @@ class Main {
       else if(args[(args.length-1)].equals("-")){
         System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
       }
+      else if(args[(args.length-1)].equals("*")){
+        System.out.println(Integer.parseInt(args[0])*Integer.parseInt(args[1]));
+      }
+      else if(args[(args.length-1)].equals("/")){
+        if (Integer.parseInt(args[1] != 0) {
+          System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));
+        } else {
+          System.out.println("Division par zéro interdite");
+        }
+      }
       else {System.exit(-1);}
     }
 }
